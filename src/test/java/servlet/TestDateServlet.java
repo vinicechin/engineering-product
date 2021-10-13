8c475c4939a6a4f395fd2eb8bfe11d389906f38b
Vinicius Cechin <vinicechin@gmail.com>
2021-10-13 20:52:52 +0200

    Add basic tests

---
+++ src/test/java/servlet/TestDateServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TestDateServlet {
+ 
+    @Test
+    public void testAbc() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testAbcd() {
+        assertEquals(3, 1 + 1 + 1);
+    }
+}
